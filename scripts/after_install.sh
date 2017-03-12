f622120a7e89586864a22cd62b3e651b7c4c817a
Mostafa Attia <freedom4soul@gmail.com>
2017-03-12 14:34:19 +0300

    [Fix] Deployment scripts   [01:00]

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-php /var/www/html/artisan clear-compiled
-php /var/www/html/artisan optimize
-php /var/www/html/artisan view:clear
-php /var/www/html/artisan cache:clear
 
-sudo chown -R ec2-user:www /var/www/html
-sudo find /var/www/html -type d -exec sudo chmod 775 {} +
+#php /var/www/html/artisan clear-compiled
+#php /var/www/html/artisan optimize
+#php /var/www/html/artisan view:clear
+#php /var/www/html/artisan cache:clear
+
+chown -R ec2-user:www /var/www/html
+#sudo find /var/www/html -type d -exec sudo chmod 775 {} +
 # sudo find /var/www/html -type f -exec sudo chmod 644 {} +
-sudo chmod -R 777 /var/www/html/storage
-sudo chmod -R 777 /var/www/html/bootstrap
-sudo chmod -R 777 /var/www/html/.env
+#sudo chmod -R 777 /var/www/html/storage
+#sudo chmod -R 777 /var/www/html/bootstrap
+#sudo chmod -R 777 /var/www/html/.env
 
-composer install -d /var/www/html
+#composer install -d /var/www/html
