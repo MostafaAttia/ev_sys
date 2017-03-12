d456b18a493c86ed8b2cb978f2fec07be4f0efb5
Mostafa Attia <freedom4soul@gmail.com>
2017-03-12 14:47:35 +0300

    [Fix] Deployment scripts   [01:00]

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -6,7 +6,7 @@
 #php /var/www/html/artisan cache:clear
 
 chown -R ec2-user:www /var/www/html
-#sudo find /var/www/html -type d -exec sudo chmod 775 {} +
+#sudo find /var/www/html -type d -exec sudo chmod 755 {} +
 # sudo find /var/www/html -type f -exec sudo chmod 644 {} +
 #sudo chmod -R 777 /var/www/html/storage
 #sudo chmod -R 777 /var/www/html/bootstrap
