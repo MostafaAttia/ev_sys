5ecf7206d4f718f45f35b751715f4f96671bec2d
Mostafa Attia <mostafa.elperrawy@gmail.com>
2017-08-07 18:29:50 +0300

    [Add] init social buttons js   [01:15]

--- public/front/js/vt-custom.js
+++ public/front/js/vt-custom.js
@@ -115,27 +115,44 @@ $(document).ready(function(){
 
 
 
-    var popupSize = {
-        width: 780,
-        height: 550
-    };
+    //var popupSize = {
+    //    width: 780,
+    //    height: 550
+    //};
+    //
+    //$('.social-share').on('click', function(e){
+    //
+    //    var
+    //        verticalPos = Math.floor(($(window).width() - popupSize.width) / 2),
+    //        horisontalPos = Math.floor(($(window).height() - popupSize.height) / 2);
+    //
+    //    var popup = window.open($(this).prop('href'), 'social',
+    //        'width='+popupSize.width+',height='+popupSize.height+
+    //        ',left='+verticalPos+',top='+horisontalPos+
+    //        ',location=0,menubar=0,toolbar=0,status=0,scrollbars=1,resizable=1');
+    //
+    //    if (popup) {
+    //        popup.focus();
+    //        e.preventDefault();
+    //    }
+    //
+    //});
+
+    function fbs_click(width, height) {
+        var leftPosition, topPosition;
+        //Allow for borders.
+        leftPosition = (window.screen.width / 2) - ((width / 2) + 10);
+        //Allow for title and status bars.
+        topPosition = (window.screen.height / 2) - ((height / 2) + 50);
+        var windowFeatures = "status=no,height=" + height + ",width=" + width + ",resizable=yes,left=" + leftPosition + ",top=" + topPosition + ",screenX=" + leftPosition + ",screenY=" + topPosition + ",toolbar=no,menubar=no,scrollbars=no,location=no,directories=no";
+        u=location.href;
+        t=document.title;
+        window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent(u)+'&t='+encodeURIComponent(t),'sharer', windowFeatures);
+        return false;
+    }
 
     $('.social-share').on('click', function(e){
-
-        var
-            verticalPos = Math.floor(($(window).width() - popupSize.width) / 2),
-            horisontalPos = Math.floor(($(window).height() - popupSize.height) / 2);
-
-        var popup = window.open($(this).prop('href'), 'social',
-            'width='+popupSize.width+',height='+popupSize.height+
-            ',left='+verticalPos+',top='+horisontalPos+
-            ',location=0,menubar=0,toolbar=0,status=0,scrollbars=1,resizable=1');
-
-        if (popup) {
-            popup.focus();
-            e.preventDefault();
-        }
-
+        return fbs_click(780, 550)
     });
 
 
